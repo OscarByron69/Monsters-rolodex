5014fe8af15da3d2a59a67479d11df577969f299
Oscar <denegbyron@gmail.com>
2020-07-14 00:46:13 -0500

    Updates

---
+++ precache-manifest.290995b5e06b3a479d4e63889809ae00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873c285f04b7b7e7fa20780e5a109c3e",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cdb37bd332f5c464cfc0",
+    "url": "/Monsters-rolodex/static/css/main.2f1aec4d.chunk.css"
+  },
+  {
+    "revision": "8b3847cb9152bfb6f1ef",
+    "url": "/Monsters-rolodex/static/js/2.12ca5671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.12ca5671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb37bd332f5c464cfc0",
+    "url": "/Monsters-rolodex/static/js/main.4a4731d2.chunk.js"
+  },
+  {
+    "revision": "e3f45222fcfa84167268",
+    "url": "/Monsters-rolodex/static/js/runtime-main.8d04db1b.js"
+  }
+]);
